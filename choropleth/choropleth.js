b153cda106184108f8c2af06a0b4d1441faf052a
MasonN808 <mason.nakamura1@marist.edu>
2022-04-28 17:33:15 -0400

    link

--- choropleth/choropleth.js
+++ choropleth/choropleth.js
@@ -1,3 +1,5 @@
 // let countyURL = 
 // let covidURL = "https://data.cdc.gov/api/views/nra9-vzzn/rows.json?accessType=DOWNLOAD"
 // let countyData;
+
+// http://bl.ocks.org/jadiehm/af4a00140c213dfbc4e6
